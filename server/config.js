adad324aa95b252266ef071d8a71409e6b6323dc
kiriti <kkomaragiri@webmd.net>
2018-08-07 16:39:54 +0530

    add mongo mlab url2

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: 3000,
-    mongoConnection: 'mongodb://localhost:27017/designstack21'
+    mongoConnection: 'mongodb://designstack21:designstack21@ds023325.mlab.com:23325/designstack21'
 }
