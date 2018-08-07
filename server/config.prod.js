9706fb11d65ebd609dc1b203e8e32b210fd172e2
kiriti <kkomaragiri@webmd.net>
2018-08-07 16:22:16 +0530

    add mongo mlab url

--- server/config.prod.js
+++ server/config.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: 8080,
-    mongoConnection: 'mongodb://<designstack21>:<Design@21>@ds033484.mlab.com:33484/designstack21'
+    mongoConnection: 'mongodb://designstack21:designstack21@ds023325.mlab.com:23325/designstack21'
 }
