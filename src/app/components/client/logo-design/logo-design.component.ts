b5dda9e3a106487370a5d7260982fc28284bc523
Kiriti K <kiriti.k999@gmail.com>
2018-09-03 09:06:06 +0530

    fix logo form submit

--- src/app/components/client/logo-design/logo-design.component.ts
+++ src/app/components/client/logo-design/logo-design.component.ts
@@ -64,7 +64,7 @@ export class LogoDesignComponent implements OnInit {
       logoDescription: '',
     });
     this.logoPackageForm = this.fb.group({
-      secondCtrl: ['', Validators.required]
+      logoPackSelected: ['', Validators.required]
     });
 
 
